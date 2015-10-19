d3b02816ac5af5ad5e5b7bad8f3c79e934b90677
Matteo Valoriani <m-atteo@hotmail.it>
2015-10-19 12:07:54 +0200

    lezione 19

---
+++ Array/Array.c
@@ -0,0 +1,23 @@
+// Array.cpp : Defines the entry point for the console application.
+//
+
+#include <stdio.h>
+#include <tchar.h>
+
+#define LUNG 5 
+	int main() {
+		int vett[LUNG], i, sum;
+		printf("Inserire un vettore di interi di dimensione %d\n", LUNG);
+		for (i = 0; i < LUNG; i++)
+			scanf("%d", &vett[i]);
+		/* Somma gli elementi del vettore */
+		for (i = 0; i < LUNG; i++) {
+			sum = sum + vett[i];
+		}
+		/* Stampa la somma */
+		printf("Somma: %d\n", sum);
+
+			return 0;
+
+}
+
